cdac26985a5e6ffb434f7b8efd7d9561904154ef
gerardkuriger <gerardkuriger@gmail.com>
2025-03-17 20:40:38 +1300

    basic constructor added

--- qrcode.cpp
+++ qrcode.cpp
@@ -18,25 +18,32 @@ using namespace std;
 class qrcode
 {
 public:
+
+    void set_version(int version)
+    {
+        if (0 < version < 41){
+            this->version = version;
+        } else {
+            printf("The version is invalid, enter number between 1 and 40");
+        }
+    }
+
+    int get_size(){ return get_size(this->version); }
+
+    int get_size(int version){ return (17 + (version * 4)); }
+
+    int get_version(){ return version; }
+
+    qrcode(string Message, int version, string type, int err_level){
+
+    }
+
+
+
+private:
     char type;
     int version;
 
-    int set_version(int version);
-
-    int get_size()
-    {
-        return get_size(get_version());
-    }
-
-    int get_size(int version)
-    {
-        return (17 + (version * 4));
-    }
-
-    int get_version()
-    {
-        return version;
-    }
 
     
 };
